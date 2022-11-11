ada6f6fa7eafe0661c26f24551e77d39c95cb61d
YeezusTheG <saringer.mark@gmail.com>
2022-11-11 13:09:10 +0100

    Update nov10.py

--- andrasi_norbert /nov10.py
+++ andrasi_norbert /nov10.py
@@ -1,17 +1,17 @@
 A = [1,2,3,4,5,6,7,8,9]
 B = 8
 C = []
-def T(a,i):
-    global masikszam
+def T(szam):
     for i in range(len(A)):
-        if a*A[i]==B:
-            masikszam = A[i]
+        if szam*A[i]==B and szam != A[i]:
+            global found_num
+            found_num = A[i]
             return True
 i = 0
 while i < len(A) and not T(A[i]):
     i = i + 1
 van = i < len(A)
 if van:
-    print("A két szám:  ", (A[i]), "és" (masikszam) )
+    print(f"A két szám: {A[i]} és {found_num}")
 else:
     print("nincs a listában a tételnek megfelelő 2 elem")
