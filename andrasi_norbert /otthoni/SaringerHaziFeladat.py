2d2d15ea23446f5769a55a24a4269501854df0c9
YeezusTheG <saringer.mark@gmail.com>
2022-12-01 10:45:02 +0100

    Update SaringerHaziFeladat.py

--- andrasi_norbert /otthoni/SaringerHaziFeladat.py
+++ andrasi_norbert /otthoni/SaringerHaziFeladat.py
@@ -1,33 +1,31 @@
-def T (i):
-    #Barmi amit megfogunk adni feltetelnek
-    return i
 #Összegzés
-def osszegzestetel(X):
-    s = 0
+def osszegzes(X, func,s=0):
     for i in range(len(X)):
-        s = s + X[i]
+        s = func(X[i])
     return s
 
 #Megszámolás
-def megszamolastetel(X):
+def megszamolas(X, T):
     c = 0
     for i in range(len(X)):
-        if T(X[i]):
+        if (T(X[i])):
             c += 1
     return c
-#Maximum-kiválasztás
-def MaxKivalasztas(X):
-    MaxErt = X[0]
-    MaxInd = 0
+
+#szelsoertek
+def szelsoertek(X, func):
+    Ert = X[0]
+    Ind = 0
     for i in range(1, len(X)):
-        if X[i] > MaxErt:
-            MaxErt = X[i]
-            MaxInd = i
-    return(MaxErt,MaxInd)
+        if (func(X[i],Ert)):
+            Ert = X[i]
+            Ind = i
+    return(Ert,Ind)
+
 #Keresés
-def kereses (X):
+def kereses (X, func):
     i = 1
-    while i <= len(X) and not T(X[i]):
+    while i <= len(X) and not func(X[i]):
         i += 1
     Van = i <= len(X)
     if Van:
@@ -36,17 +34,17 @@ def kereses (X):
     return(Index, Ertek)
 
 #Eldőntés
-def eldontes (X):
+def eldontes (X, func):
     i = 1
-    while i <= len(X) and not T(X[i]):
+    while i <= len(X) and not func(X[i]):
         i += 1
     Van = i <= len(X)
     return Van
 
 #Kiválasztás
-def kivalasztas (X):
+def kivalasztas (X, func):
     i = 1
-    while not T(X[i]):
+    while not func(X[i]):
         i += 1
     Index = i
     Ertek = X[i]
