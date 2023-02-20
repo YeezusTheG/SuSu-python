97adc07f3c46ee13f95c2bada76ed99f98a4fd17
YeezusTheG <saringer.mark@gmail.com>
2023-02-20 09:14:33 +0100

    Update B3.py

--- andrasi_norbert /orai/agazatigyakorlo/B3.py
+++ andrasi_norbert /orai/agazatigyakorlo/B3.py
@@ -1,11 +1,28 @@
 
 def adatokBeolvasasa(fajlnev):
-    adatok = {}
-    with open("andrasi_norbert //orai//agazatigyakorlo//fuvarok.txt") as f:
+    with open(fajlnev, "r") as f:
+        adatok = []
         for sor in f:
             sor = sor.strip().split()
             rendszam = sor[0]
-            kilometerszamok = [int(x) for x in sor[1:]]
-            adatok[rendszam] = kilometerszamok
+            futasteljesitmenyek = []
+            for i in range(1, len(sor)):
+                try:
+                    futasteljesitmenyek.append(int(sor[i]))
+                except ValueError:
+                    futasteljesitmenyek.append(0)
+            adatok.append([rendszam] + futasteljesitmenyek)
     return adatok
 
+def nullaKm(adatok):
+    szamlalo = 0
+    for i in adatok[1:]:
+        if i == 0:
+            szamlalo += 1
+    return szamlalo
+
+
+adatok = adatokBeolvasasa("andrasi_norbert //orai//agazatigyakorlo//fuvarok.txt")
+print(adatok)
+nullak_szama = nullaKm(adatok)
+print(nullak_szama)
